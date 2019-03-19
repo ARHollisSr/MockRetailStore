d8a724f85776cbe45d79c5fba3ec3f5dada007ac
FocusedBit <anthonyrayhollis@gmail.com>
2019-03-19 19:59:17 -0400

    added bootstrap-flatly

--- ThreeHitCombo/App_Start/BundleConfig.cs
+++ ThreeHitCombo/App_Start/BundleConfig.cs
@@ -23,7 +23,7 @@ namespace ThreeHitCombo
                       "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrap-flatly.css",
                       "~/Content/site.css"));
         }
     }
