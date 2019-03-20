eb42a121807f77c626448db787e47bb9cb2823ed
FocusedBit <anthonyrayhollis@gmail.com>
2019-03-19 20:16:17 -0400

    Adding new bootstrap theme files

--- ThreeHitCombo/App_Start/BundleConfig.cs
+++ ThreeHitCombo/App_Start/BundleConfig.cs
@@ -23,7 +23,7 @@ namespace ThreeHitCombo
                       "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap-flatly.css",
+                      "~/Content/bootstrap-superhero.css",
                       "~/Content/site.css"));
         }
     }
