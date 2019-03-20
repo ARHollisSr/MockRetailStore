1b622762d40fc9caa55d7ba5ab68755ff4cd4298
FocusedBit <anthonyrayhollis@gmail.com>
2019-03-20 18:19:49 -0400

    updated files

---
+++ ThreeHitCombo/Controllers/GamesController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ThreeHitCombo.Models;
+
+namespace ThreeHitCombo.Controllers
+{
+    public class GamesController : Controller
+    {
+        // GET: Game/Random
+        public ActionResult Random()
+        {
+            var game = new Game() {Name = "Fortnite!"};
+            return View(game);
+
+        }
+
+        public ActionResult Edit(int id)
+        {
+            return Content("id= " + id);
+        }
+
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+            {
+                pageIndex = 1;
+            }
+            if (String.IsNullOrWhiteSpace(sortBy))
+            {
+                sortBy = "Name";
+            }
+
+            return Content($"pageIndex={pageIndex}&sortBy={sortBy}");
+
+        }
+    }
+}
